5b02b09b86dd1f25962682d995b975f19bcca20f
Gregorio Minetto <117943888+S5679962@users.noreply.github.com>
2022-12-08 23:19:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char c;
+  char v;
+  cin>>c;
+  if (checkimput(x: c)){
+    v=conversione(X: c);
+    cout<<v;
+ }else{
+    cout << "errore" << endl;
+ }
   return 0;
 }
